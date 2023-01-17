0dfefca68aa0bd5d56505f242cb1cdb292979d7b
deepa.singh <deepa.singh@host.docker.internal>
2023-01-17 15:30:15 +0530

    first commit

---
+++ GradleDemo/src/main/java/com/demo/hello/controller/HelloController.java
@@ -0,0 +1,9 @@
+/*
+ * package com.demo.hello.controller;
+ * 
+ * @RestController public class HelloController {
+ * 
+ * @GetMapping("\hi") public String sayHi() { return "Welcome to my page"; }
+ * 
+ * }
+ */
